98670d06ac1b4c021c75f2a012197c323fca1320
Ahmed-Gamil-osman <ahmed.gamil@student.aast.edu>
2025-09-20 12:15:03 +0300

    added the travis file for CI

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+test('renders Learn from Gamil text twice', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const elements = screen.getAllByText(/learn from gamil/i);
+  expect(elements).toHaveLength(2);
 });
 
 
+test('renders Learn from Gamil text twice', () => {
+  render(<App />);
+  const elements = screen.getAllByText(/learn from gamil/i);
+  expect(elements).toHaveLength(2);
+});
 
+test('renders Learn from Gamil text twice', () => {
+  render(<App />);
+  const elements = screen.getAllByText(/learn from gamil/i);
+  expect(elements).toHaveLength(2);
+});

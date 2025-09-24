0baaa8cf8b82c484de614a29bd86ec51fc239535
Ahmed-Gamil-osman <ahmed.gamil@student.aast.edu>
2025-09-24 19:56:37 +0300

    changed some test cases

--- src/App.test.js
+++ src/App.test.js
@@ -15,28 +15,5 @@ test('renders Learn from Gamil text twice', () => {
   expect(elements).toHaveLength(2);
 });
 
-test('renders Learn from Gamil text twice', () => {
-  render(<App />);
-  const elements = screen.getAllByText(/learn from gamil/i);
-  expect(elements).toHaveLength(2);
-});
 
-test('renders Learn from Gamil text twice', () => {
-  render(<App />);
-  const elements = screen.getAllByText(/learn from gamil/i);
-  expect(elements).toHaveLength(2);
-});
-
-
-test('renders Learn from Gamil text twice', () => {
-  render(<App />);
-  const elements = screen.getAllByText(/learn from gamil/i);
-  expect(elements).toHaveLength(2);
-});
-
-test('renders Learn from Gamil text twice', () => {
-  render(<App />);
-  const elements = screen.getAllByText(/learn from gamil/i);
-  expect(elements).toHaveLength(2);
-});
 

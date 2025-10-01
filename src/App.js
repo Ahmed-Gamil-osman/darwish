51f7f0c07236ac8a0aebeb6462db77353e2c6fba
Ahmed-Gamil-osman <ahmed.gamil@student.aast.edu>
2025-10-01 22:33:30 +0300

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Learn from Gamil  
+          ❤️ مورجانتي حبيبتي محور كوني ❤️ 
         </p>
         <a
           className="App-link"

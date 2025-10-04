0f4a592bdaa65706ecaba49bc5c5d09c12d9ec3e
Ahmed-Gamil-osman <ahmed.gamil@student.aast.edu>
2025-10-04 10:07:36 +0300

    this is the new env

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ❤️ مورجانتي حبيبتي محور كوني ❤️ 
+          Learn from Gamil 
         </p>
         <a
           className="App-link"
